b6e45ba726c76dcaa469e1fce48eec3eae4588c3
Ben <hubertben365@gmail.com>
2022-06-30 15:48:51 -0400

    tkinter

--- client.py
+++ client.py
@@ -1,7 +1,29 @@
 
+import tkinter as tk
 import socket
 
+port = None
+host = None
+
 if __name__ == "__main__":
+
+    kinter = tk.Tk()
+    kinter.title("Client")
+    kinter.geometry("300x300")
+    kinter.resizable(False, False)
+    kinter.configure(background="gray")
+
+    # add text input field to the window
+    text_input = tk.Entry(kinter, width=30)
+    text_input.place(x=50, y=50)
+
+    # add button to the window
+    button = tk.Button(kinter, text="Send", width=10, command=lambda: send_message(text_input.get()))
+    button.place(x=50, y=100)
+
+    # add label to the window
+    
+
 
     port = int(input("Enter port number: "))
 
@@ -12,30 +34,41 @@ if __name__ == "__main__":
     num = int(input("Enter host number: "))
     host = "elnux" + str(num) + ".cs.umass.edu"
     server_id = -1
+
     
 
-    while True:
-        s = socket.socket()
-        s.connect((host, port))
+
+def send_message(m):
+    s = socket.socket()
+    s.connect((host, port))
+    s.send(m.encode())
+    r = s.recv(1024).decode()
+
+    label = tk.Label(kinter, text=r)
+    label.place(x=50, y=200)
+
+
+    if(r[0] == "$"):
+        server_id = int(r[1])
+        print("Server ID: " + str(server_id))
+    
+    s.close()
+
         
-        m = input("Enter message: ")
 
-        s.send(m.encode())
 
-        r = s.recv(1024).decode()
-        print("Response:\t", str(r))
+    
 
         
-        if(r[0] == "$"):
-            server_id = int(r[1])
-            print("Server ID: " + str(server_id))
+
+        
+        
+
+        
+        
 
        
-        s.close()
-
-        if(str(m) == "quit"):
-            break
-
+        
         
 
     
