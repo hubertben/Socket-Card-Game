73f8e001bd8f25f32f8edae84d02b3a5227387f3
Ben <hubertben365@gmail.com>
2022-06-30 12:41:09 -0400

    bug fix

--- server.py
+++ server.py
@@ -3,24 +3,24 @@ import socket
 
 if __name__ == "__main__":
     
-    port = int(input("Enter port number: ")) # Used While Testing: 19487
+    port = int(input("Enter port number: "))
    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostname() # Get local machine name
-    s.bind((host, port))  # Bind to the port
-
-    s.listen(5)  # Now wait for client connection
+    host = socket.gethostname() 
+    s.bind((host, port))  
+    s.listen(5)  
     print("Server listening @ {}:{}".format(host, port))
+    
     while True:
-        c, addr = s.accept()  # Establish connection with client
-        print(f"Connection from {addr} has been established.")
-        m = c.recv(1024).decode() # Get the message
+        c, addr = s.accept()  
+        print("Connection from " + addr + " has been established.")
+        m = c.recv(1024).decode() 
         
         print("Request:\t", m)
 
         s = "Testing"
 
-        c.send(s.encode())  # Send the response
+        c.send(s.encode())
         
-        c.close()  # Close the connection
+        c.close() 
         
